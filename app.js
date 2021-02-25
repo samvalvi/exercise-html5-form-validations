1315b0f732c1c838a27e169150c9d536d63bd103
Samuel <samuelvalerin@gmail.com>
2021-02-25 17:16:10 -0600

    shadow added

---
+++ app.js
@@ -0,0 +1,11 @@
+let cardNumber = document.getElementById('card-number').value;
+let cvc = document.getElementById('cvc').value;
+let amount = document.getElementById('amount').value;
+let firstName = document.getElementById('firstname').value;
+let lastName = document.getElementById('lastname').value;
+let city = document.getElementById('city').value;
+let state = document.getElementById('state').value;
+
+const formValiation = () => {
+    
+}
