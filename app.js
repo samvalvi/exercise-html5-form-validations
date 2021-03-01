203698454e7a088cc07be66c33d840044cdc2b11
Samuel <samuelvalerin@gmail.com>
2021-03-01 10:47:05 -0600

    errorAlert finished

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ form.addEventListener('submit', (event)=> {
     if(cardNumber.trim() === '' || cvc.trim() === '' || amount.trim() === '' || firstName.trim() === '' || lastName.trim() === '' ||
        city.trim() === '' || state.trim() === '' || textarea.trim() === '') {
         console.log('Some fields are missing');
+
+        let parentContainer = document.getElementById('alert');
+        let errorAlert = document.createElement('div');
+        
+        let html = `<div class="m-4 alert alert-danger" role="alert">
+                        Some fields are missing
+                    </div>`
+
+        errorAlert.innerHTML = html;
+        parentContainer.appendChild(errorAlert);
         return;
     }
 
