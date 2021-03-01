06eec04370abe111a3d9272d27fb171d3e7fc4fe
Samuel <samuelvalerin@gmail.com>
2021-03-01 10:02:52 -0600

    addEventListener modified

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-let cardNumber = document.getElementById('card-number').value;
-let cvc = document.getElementById('cvc').value;
-let amount = document.getElementById('amount').value;
-let firstName = document.getElementById('firstname').value;
-let lastName = document.getElementById('lastname').value;
-let city = document.getElementById('city').value;
-let state = document.getElementById('state').value;
+let form = document.querySelector("#myForm");
 
-const formValiation = () => {
-    
-}
+form.addEventListener('submit', (event)=> {
+
+    event.preventDefault();
+
+    let cardNumber = document.getElementById('card-number').value;
+    let cvc = document.getElementById('cvc').value;
+    let amount = document.getElementById('amount').value;
+    let firstName = document.getElementById('firstname').value;
+    let lastName = document.getElementById('lastname').value;
+    let city = document.getElementById('city').value;
+    let state = document.getElementById('state').value;
+    let textarea = document.getElementById('messages').value;
+
+    if(cardNumber.trim() === '' || cvc.trim() === '' || amount.trim() === '' || firstName.trim() === '' || lastName.trim() === '' ||
+       city.trim() === '' || state.trim() === '' || textarea.trim() === '') {
+        console.log('Some fields are missing');
+        return;
+    }
+
+})
